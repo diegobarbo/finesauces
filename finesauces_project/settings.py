8ce1fe941545e6a125ebd2274c8fc4fae58f8910
Diego Barbo <diegobarbox@gmail.com>
2022-04-01 23:17:57 -0300

    listings app created

--- finesauces_project/settings.py
+++ finesauces_project/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -31,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'listings.apps.ListingsConfig',
 ]
 
 MIDDLEWARE = [
