8992666a7e1893571f1b3765199dcddb0bfcbdbf
Diego Barbo <diegobarbox@gmail.com>
2022-04-02 00:19:22 -0300

    adding models to the administration site

--- listings/models.py
+++ listings/models.py
@@ -7,6 +7,10 @@ class Category(models.Model):
 
     class Meta:
         ordering = ('-name',)
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -21,3 +25,6 @@ class Product(models.Model):
 
     class Meta:
         ordering = ('shu',)
+
+    def __str__(self):
+        return self.name
